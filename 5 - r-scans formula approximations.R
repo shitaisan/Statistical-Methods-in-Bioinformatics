530bbcac2a7ceaaf2b2cf88709fa36a990d50454
shitaisan <35894037+shitaisan@users.noreply.github.com>
2020-02-28 17:07:47 +0300

    rmpfr for your soul

--- 5 - r-scans formula approximations.R
+++ 5 - r-scans formula approximations.R
@@ -1,5 +1,6 @@
+library(Rmpfr)
 N <- c(1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9)
-u <- 0.00001
+u <- 1e-3
 h <- ceiling(1/u-1)
 g <- 1:h
 
@@ -8,7 +9,7 @@ approx1 <- c()
 approx2 <- c()
 for (n in N){
   #probability of Umax>u
-  exact <- c(exact, sum((-1)^(g+1)*choose(n+1, g)*(1-g*u)^n))
+  exact <- c(exact, sum((-1)^(g+1)*chooseMpfr(n+1, g)*mpfr(1-g*u, precBits = 1000)^n)
   approx1 <- c(approx1, 1-exp(-(n+1)*exp(-(n+1)*u)))
   approx2 <- c(approx2, (n+1)*exp(-(n+1)*u))
 }
