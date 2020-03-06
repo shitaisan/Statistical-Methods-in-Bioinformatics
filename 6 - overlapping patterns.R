8a61398bb1208d96ff9f48de0fe7718e8527c202
shitaisan <35894037+shitaisan@users.noreply.github.com>
2020-03-06 17:12:23 +0300

    Update 6 - overlapping patterns.R

--- 6 - overlapping patterns.R
+++ 6 - overlapping patterns.R
@@ -1,7 +1,7 @@
-N <- 3160000
-w <- "gaga"
-p <- c('a' = 1/4, 'c' = 1/4, 'g' = 1/4, 't' = 1/4)
-DNA <- sample(c('a', 'c', 'g', 't'), N, replace=T, prob = p)
+N <- 1e8
+w <- "gggggggggg"
+prob <- c('a' = 1/6, 'c' = 1/6, 'g' = 1/2, 't' = 1/6)
+DNA <- sample(c('a', 'c', 'g', 't'), N, replace=T, prob = prob)
 
 textDNA <- paste(DNA, collapse = '')
 N <- nchar(textDNA)
@@ -18,16 +18,18 @@ for (i in 1:(length(x)-1)){
 }
 
 plot(ecdf(count))
+p <- 1-1/mean(count)
+curve(pgeom(x, prob = p), add = T)
 
 # ============ number of letters before first appearance
-
-N <- 1000
+# install.packages("stringi")
+w <- 'gaga'
+N <- 100000
 begin <- substring(w, 1, 1:nchar(w))
 end <- substring(w, nchar(w):1, nchar(w))
 eps <- begin==end
-N <- 
 test <- function(x){
-  DNA <- sample(c('a', 'c', 'g', 't'), N, replace=T, prob = p)
+  DNA <- sample(c('a', 'c', 'g', 't'), N, replace=T, prob = prob)
   textDNA <- paste(DNA, collapse = '')
   return (stringi::stri_locate_first_fixed(textDNA, w)[,1])
 }
@@ -36,7 +38,7 @@ res  <- sapply(1:1000, test)
 
 mean(res, na.rm = T)
 # exact mean  
-temp <- eps/sapply(begin, function(x) prod(p[strsplit(x, split = NULL)[[1]]]))
+temp <- eps/sapply(begin, function(x) prod(prob[strsplit(x, split = NULL)[[1]]]))
 print(m <- sum(temp))
 var(res, na.rm = T)
 m^2+m-2*sum(1:nchar(w)*temp)
