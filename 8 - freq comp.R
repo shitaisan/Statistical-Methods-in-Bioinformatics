8aeb53ccee0caff5ba59f87e70465a34555c72d7
ShitaiDSCH <t0bi0rit3iku@gmail.com>
2020-03-12 11:46:55 +0300

    hm

---
+++ 8 - freq comp.R
@@ -0,0 +1,18 @@
+N <- 10000
+p <- c('a' = 1/4, 'c' = 1/4, 'g' = 1/4, 't' = 1/4)
+nucls <- c('a', 'c', 'g', 't')
+
+test <- function(k){
+  DNA <- sample(nucls, N, replace=T, prob = p)
+  statistic <- function(n){
+    freqs <- rbind(table(DNA[1:n]), table(DNA[(n+1):N]))
+    freqs <- rbind(freqs, apply(freqs, 2, sum))
+    return (2*sum(freqs[1,]*log(freqs[1,]/n))
+            +2*sum(freqs[2,]*log(freqs[2,]/(N-n)))
+            -2*(sum(freqs[3,]*log(freqs[3,]/N))))
+  }
+  return (max(sapply(50:(N-50), statistic)))
+}
+
+res <- sapply(1:10, test)
+warnings()
