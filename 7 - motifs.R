f0f3953a9fd355f4690c241e8b3e1d10613ea8e0
ShitaiDSCH <t0bi0rit3iku@gmail.com>
2020-03-14 11:58:53 +0300

    three fors im so sorry

--- 7 - motifs.R
+++ 7 - motifs.R
@@ -1,4 +1,4 @@
-N <- 1000000
+N <- 100000
 p <- c('a' = 1/4, 'c' = 1/4, 'g' = 1/4, 't' = 1/4)
 nucls <- c('a', 'c', 'g', 't')
 M <- c('gatggtgg', 'gctggtgg', 'ggtggtgg', 'gttggtgg')
@@ -22,6 +22,8 @@ for (k in 1:100){
   mean_dist_between_occur <- c(mean_dist_between_occur, 
                                mean(all[2:l]-all[1:(l-1)]))
 }
+
+#check means of motif number and distance between occurences
 #=========
 mean(motifnumb)
 probM <- sum(sapply(M, function(w) 
@@ -31,13 +33,28 @@ probM <- sum(sapply(M, function(w)
 mean(mean_dist_between_occur)
 1/probM
 
+# check var of motif number
+#==========
 eps <- array(dim = c(m, m, k), dimnames = list(M, M, 1:k))
 for (u in M){
   for (v in M){
-    begin <- substring(u, 1, 1:nchar(u))
-    end <- substring(v, nchar(v):1, nchar(v))
+    begin <- substring(v, 1, 1:nchar(v))
+    end <- substring(u, nchar(u):1, nchar(u))
     eps[u, v, ] <- begin==end
   }
 }
 
-# NEXT !
+pi <- array(dim = c(m, m, k), dimnames = list(M, M, 1:k))
+for (u in M){
+  u1 <- strsplit(u, split='')[[1]]
+  for (v in M){
+    v1 <- strsplit(v, split='')[[1]]
+    for (j in 1:k)
+        pi[u, v, j] <- prod(p[u1], p[v1])/prod(p[v1[1:j]])
+  }
+}
+
+var(motifnumb)
+(N-k+1)*probM-((2*k-1)*N-3*k^2+4*k-1)*probM^2+2*sum((N-2*k+1:(k-1)+1)*apply(pi*eps, 3, sum)[-k])
+  
+  
